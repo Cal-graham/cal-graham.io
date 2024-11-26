32a09d25ade3af9c076107df00d4ffc3c36a5511
Cal <45156674+Cal-graham@users.noreply.github.com>
2024-11-26 15:02:39 +0000

    Syntax fix portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -34,7 +34,7 @@ const greeting = {
 
 const socialMediaLinks = {
   github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
+  linkedin: "https://www.linkedin.com/in/saadpasta/"
   //gmail: "saadpasta70@gmail.com",
   //gitlab: "https://gitlab.com/saadpasta",
   //facebook: "https://www.facebook.com/saad.pasta7",
