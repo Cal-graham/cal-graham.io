f8435113993e210fb8ad6f27bbc8462ff9ce326e
Cal <45156674+Cal-graham@users.noreply.github.com>
2024-12-02 08:51:29 -0500

    Update ToggleSwitch.js

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -5,14 +5,14 @@ import "./ToggleSwitch.scss";
 
 const ToggleSwitch = () => {
   const {isDark} = useContext(StyleContext);
-  const [isChecked, setChecked] = useState(isLight);
+  const [isChecked, setChecked] = useState(isDark);
   const styleContext = useContext(StyleContext);
 
   return (
     <label className="switch">
       <input
         type="checkbox"
-        checked={isLight}
+        checked={isDark}
         onChange={() => {
           styleContext.changeTheme();
           setChecked(!isChecked);
